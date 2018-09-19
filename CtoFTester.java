61765dd0a8580d5f13285204a0948d6dc5f892f5
Jack Lam <jlam00@stuy.edu>
2018-09-18 22:18:15 -0400

    Added main and functions and works

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,3 +1,12 @@
 public class CtoFTester{
-    
+  public static void main(String[] args){
+    System.out.println (CtoF(30.0));
+    System.out.println (FtoC(41.0));
 }
+
+public static double CtoF(double c){
+  return ((c * 9.0)/5.0) + 32.0;
+}
+public static double FtoC(double f){
+  return ((f - 32.0) * 5.0) / 9.0;
+}}
